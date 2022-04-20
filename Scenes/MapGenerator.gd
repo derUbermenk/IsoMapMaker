454b8cb6b6958a4e367fd25af8b4f51f140cbf36
Chester Asuncion <asuncionmcv@gmail.com>
2022-04-20 13:06:10 +0800

    add isometric hex grid builder logic

--- Scenes/MapGenerator.gd
+++ Scenes/MapGenerator.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var tile_scene = load("res://Scenes/Tile.tscn")
+var hex_tile_coord = "res://Scenes/HexTile.tscn"
+var tile_coord = "res://Scenes/Tile.tscn"
+var tile_scene = load(hex_tile_coord)
 
 # represents number of tiles
 var world_size = 5 
@@ -9,6 +11,7 @@ var world_size = 5
 # isometric boxes have 2:1 width:height ratio
 var tile_width = 256 
 var tile_height = 128 
+var y_off = 0.85 # didn't know how I came up with this
 
 onready var sorter = get_node("YSort")
 
@@ -22,9 +25,30 @@ func _process(delta):
 	track_mouse_loc()
 
 func draw_world(): 
-	for x in world_size:
-		for y in world_size:
-			draw_tile(x, y)
+	for y in world_size:
+		for x in world_size:
+			if y % 2 != 0:
+				x -= 0.5	
+			draw_hex_tile(x, y)
+
+func draw_hex_tile(x, y):
+	# resize i_hat and j_hat vectors accoriding to tile width and height	
+		# divide tile_width by 2 since original takes more than 2 reference tiles
+		# ref tile is 1:1
+	var i_hat = Vector2(1 * tile_width / 2, 0.5 * tile_height)
+	var j_hat = Vector2(-1 * tile_width / 2, 0.5 * tile_height)
+	# note tile position/pivot is in bottom left of sprite
+
+
+	var tile_position: Vector2 = (x * i_hat) + (y * y_off * j_hat)
+
+	var tile = tile_scene.instance()
+	tile.position = tile_position
+
+	# YSort arranges draw order by y position. 
+	get_node("YSort").add_child(tile)
+
+
 
 func draw_tile(x, y):
 	# resize i_hat and j_hat vectors accoriding to tile width and height	
@@ -44,5 +68,3 @@ func draw_tile(x, y):
 
 func track_mouse_loc():
 	var mouse_pos = get_global_mouse_position()
-	print("x: ", mouse_pos.x)
-	print("y: ", mouse_pos.y, "\n")
